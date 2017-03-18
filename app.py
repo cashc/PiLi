e8f7bad4efbb9a60d7d0fc2b4fcc521a207b0b22
cashc <cashc@cash.me>
2017-03-18 13:35:11 -0400

    PiLi Home shows RGB inputs from URL /r,g,b,a

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask import render_template
-import os
-from flask import send_from_directory
+# import os
+# from flask import send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<red>.<green>.<blue>.<power>')
-def index(red=0, green=0, blue=0, power=0):
+@app.route('/<red>,<green>,<blue>,<power>')
+def index(red=255, green=255, blue=255, power=1):
     return render_template('index.html', r=red, g=green, b=blue, p=power)
 
-@app.route('/favicon.png')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png')

383148745c23ce99d1f458286b8a5fa7a026065b
cashc <cashc@cash.me>
2017-03-18 18:37:43 -0400

    Using LED object to store rgba ints.

--- LED.py
+++ LED.py
@@ -1,12 +1,25 @@
-import RPi.GPIO as GPIO
-import time
+# import RPi.GPIO as GPIO
+# import time
+import pigpio
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print("LED on")
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
+class LED():
+    red = 0
+    green = 0
+    blue = 0
+    power = 1
 
-print("LED off")
-GPIO.output(18,GPIO.LOW)
+    def __init__(self, red: int, green: int, blue: int, power: int):
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.power = power
+
+    def update(self, red, green, blue, power):
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.power = power
+
+# pi = pigpio.pi()
+# pi.set_PWM_dutycycle(PIN, BRIGHTNESS)
+# pi.stop()
--- app.py
+++ app.py
@@ -1,11 +1,28 @@
+from LED import LED
 from flask import Flask
 from flask import render_template
 # import os
 # from flask import send_from_directory
 app = Flask(__name__)
+lights = LED(20, 90, 150, 1)
 
 @app.route('/')
-@app.route('/<red>,<green>,<blue>,<power>')
-def index(red=255, green=255, blue=255, power=1):
-    return render_template('index.html', r=red, g=green, b=blue, p=power)
+@app.route('/<int:power>')
+@app.route('/<int:red>,<int:green>,<int:blue>')
+@app.route('/<int:red>,<int:green>,<int:blue>,<int:power>')
+def index(red=None, green=None, blue=None, power=None):
+    # only update color if a color is specified and max is 255
+    if red is None: red = lights.red
+    elif red>255: red=255
+    if green is None: green = lights.green
+    elif green>255: green=255
+    if blue is None: blue = lights.blue
+    elif blue>255: blue=255
+    if power is None: power = lights.power
+    elif power>1 and power<=100: power/=100
+    else: power = 1
+
+    lights.update(red, green, blue, power)
+
+    return render_template('index.html', lights=lights)
 

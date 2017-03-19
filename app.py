e0d137b1454c36d304200e22f2ae242fafc33a2a
cashc <cashc@cash.me>
2017-03-19 09:44:32 -0400

    Border around PiLi Home and background when RGB > 700.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import LED
+from LED import LED
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-lights = LED.LED(20, 90, 150, 1)
+lights = LED(20, 90, 150, 1)
 
 @app.route('/')
 @app.route('/<int:power>')
@@ -13,10 +13,13 @@ def index(red=None, green=None, blue=None, power=None):
     # only update color if a color is specified and max is 255
     if red is None: red = lights.red
     elif red>255: red=255
+
     if green is None: green = lights.green
     elif green>255: green=255
+
     if blue is None: blue = lights.blue
     elif blue>255: blue=255
+
     if power is None: power = lights.power
     elif power>1 and power<=100: power/=100
     else: power = 1

6e7e44659e8601b0aa477965d5dc53ffdd83ee48
cashc <cashc@cash.me>
2017-04-05 19:13:38 -0400

    Working on transition.

--- LED.py
+++ LED.py
@@ -1,9 +1,11 @@
 import pigpio
+import time
 
 pi = pigpio.pi()
 r = 17
 g = 22
 b = 24
+
 
 class LED():
     red = 0
@@ -24,13 +26,24 @@ class LED():
         self.power = power
 
     def gpio(self):
-        pi.set_PWM_dutycycle(r, self.red*self.power)
-        pi.set_PWM_dutycycle(g, self.green*self.power)
-        pi.set_PWM_dutycycle(b, self.blue*self.power)
+        pi.set_PWM_dutycycle(r, self.red * self.power)
+        pi.set_PWM_dutycycle(g, self.green * self.power)
+        pi.set_PWM_dutycycle(b, self.blue * self.power)
+
+    def transition(self, toRed, toGreen, toBlue, toPower):
+        rDiff = abs(self.red - toRed)/100
+        gDiff = abs(self.green - toGreen)/100
+        bDiff = abs(self.blue - toBlue)/100
+        pDiff = abs(self.power - toPower)/100
+        for i in range(100):
+            time.sleep(.05)
+            pi.set_PWM_dutycycle(r, (self.red + (rDiff*i)) * (pDiff*i))
+            pi.set_PWM_dutycycle(g, (self.green + (gDiff*i)) * (pDiff*i))
+            pi.set_PWM_dutycycle(b, (self.blue + (bDiff*i)) * (pDiff*i))
 
     def off(self):
-        pi.set_PWM_dutycycle(r,0)
-        pi.set_PWM_dutycycle(g,0)
-        pi.set_PWM_dutycycle(b,0)
+        pi.set_PWM_dutycycle(r, 0)
+        pi.set_PWM_dutycycle(g, 0)
+        pi.set_PWM_dutycycle(b, 0)
 
 # pi.stop()
--- app.py
+++ app.py
@@ -3,27 +3,37 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-lights = LED(5, 30, 15, .3)
+lights = LED(200, 0, 200, 50)
 lights.gpio()
+
 
 @app.route('/')
 @app.route('/<int:power>')
 @app.route('/<int:red>,<int:green>,<int:blue>')
 @app.route('/<int:red>,<int:green>,<int:blue>,<int:power>')
-def index(red=None, green=None, blue=None, power=None):
+def index(red=None, green=None, blue=None, power=None, function=None):
     # only update color if a color is specified and max is 255
-    if red is None: red = lights.red
-    elif red>255: red=255
+    if red is None:
+        red = lights.red
+    elif red > 255:
+        red = 255
 
-    if green is None: green = lights.green
-    elif green>255: green=255
+    if green is None:
+        green = lights.green
+    elif green > 255:
+        green = 255
 
-    if blue is None: blue = lights.blue
-    elif blue>255: blue=255
+    if blue is None:
+        blue = lights.blue
+    elif blue > 255:
+        blue = 255
 
-    if power is None: power = lights.power
-    elif power>1 and power<=100: power/=100
-    else: power = 1
+    if power is None:
+        power = lights.power
+    elif power > 1 and power <= 100:
+        power /= 100
+    elif power > 100:
+        power = 1
 
     lights.update(red, green, blue, power)
 
@@ -33,4 +43,10 @@ def index(red=None, green=None, blue=None, power=None):
     else:
         lights.gpio()
 
+    return render_template('index.html', lights=lights)
+
+
+@app.route('/transition,<int:red>,<int:green>,<int:blue>,<int:power>')
+def transition(red, green, blue, power):
+    lights.transition(red, green, blue, power)
     return render_template('index.html', lights=lights)

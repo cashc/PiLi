aecba19c4e1a7a488c4c14d4988db36e2a6f711c
cashc <compto51@msu.edu>
2017-04-05 21:02:01 -0400

    Transition working correctly @route to:r,g,b

--- LED.py
+++ LED.py
@@ -30,7 +30,7 @@ class LED():
         pi.set_PWM_dutycycle(g, self.green * self.power)
         pi.set_PWM_dutycycle(b, self.blue * self.power)
 
-    def transition(self, toRed, toGreen, toBlue, speed):
+    def transition(self, toRed, toGreen, toBlue, speed=20):
         rBeg = self.red
         gBeg = self.green
         bBeg = self.blue
@@ -42,18 +42,21 @@ class LED():
         step = -1
         while(self.red == rBeg and self.green == gBeg and self.blue == bBeg):
             if step > 0:
-                step = 1
-            else:
+                beg = 100
+                end = 0
                 step = -1
-
-            for i in range(0,100,step):
+            else:
+                beg = 0
+                end = 100
+                step = 1
+            for i in range(beg,end,step):
                 red = rBeg + i*rDiff
                 green = gBeg + i*gDiff
                 blue = bBeg + i*bDiff
-                time.sleep(speed/100)
                 pi.set_PWM_dutycycle(r, red * self.power)
                 pi.set_PWM_dutycycle(g, green * self.power)
                 pi.set_PWM_dutycycle(b, blue * self.power)
+                time.sleep(speed / 1000)
 
     def off(self):
         pi.set_PWM_dutycycle(r, 0)
